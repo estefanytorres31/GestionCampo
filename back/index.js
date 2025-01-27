adc488cfcbbbfd268e4cca0a11f5a00f97f99ea3
Estefany Torres <estefanyt3101@gmail.com>
2025-01-27 09:17:05 -0500

    Nuevo

---
+++ back/index.js
@@ -0,0 +1,14 @@
+import app from "./src/app.js";
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
+
+process.on('SIGTERM', () => {
+    console.log('Closing server...');
+    server.close(() => {
+        console.log('Server closed');
+        process.exit(0);
+    });
+});
