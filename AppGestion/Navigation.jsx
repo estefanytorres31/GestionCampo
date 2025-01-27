ab4cfe976ecd68ba6ed41462c36864649caac672
Angierc25 <carhuas2005@gmail.com>
2025-01-27 10:54:54 -0500

    clientes

--- AppGestion/Navigation.jsx
+++ AppGestion/Navigation.jsx
@@ -2,14 +2,16 @@ import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LoginScreen from "./screens/views/Auth/Login";
+import ClientScreen from "./screens/client/Client";
 
 const Stack = createNativeStackNavigator();
 
 export default function Navigation () {
     return (
         <NavigationContainer >
-            <Stack.Navigator initialRouteName="Login"  screenOptions={{ headerShown: false }}>
+            <Stack.Navigator initialRouteName="Clientes"  screenOptions={{ headerShown: false }}>
                 <Stack.Screen name="Login" component={LoginScreen} />
+                <Stack.Screen name="Clientes" component={ClientScreen}  options={{ headerShown: true }} />
             </Stack.Navigator>
         </NavigationContainer>
     );
