119176fbff231405acff44fd67c4756599a16177
Estefany Torres <estefanyt3101@gmail.com>
2025-01-27 12:06:09 -0500

    Nuevo

--- AppGestion/Navigation.jsx
+++ AppGestion/Navigation.jsx
@@ -2,8 +2,8 @@ import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LoginScreen from "./screens/views/Auth/Login";
-import ClientScreen from "./screens/client/Client";
-import EmbarcacionesScreen from "./screens/embarcacion/Embarcacion";
+import ClientScreen from "./screens/views/client/Client";
+import EmbarcacionesScreen from "./screens/views/embarcacion/Embarcacion";
 import { TouchableOpacity } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
@@ -12,16 +12,10 @@ const Stack = createNativeStackNavigator();
 export default function Navigation () {
     return (
         <NavigationContainer >
-            <Stack.Navigator initialRouteName="Exalmar"  screenOptions={{ headerShown: false }}>
+            <Stack.Navigator initialRouteName="Login"  screenOptions={{ headerShown: false }}>
                 <Stack.Screen name="Login" component={LoginScreen} />
-                <Stack.Screen name="Clientes" component={ClientScreen}  options={{ headerShown: true }} />
-                <Stack.Screen name="Exalmar" component={EmbarcacionesScreen}  options={{
-                headerShown: true,
-                headerTitle: "Exalmar",
-                headerLeft: () => (
-                <Ionicons name="chevron-back" size={24} color="black" />
-                ),
-                }}
+                <Stack.Screen name="Clientes" component={ClientScreen}  options={{ title: Clientes }} />
+                <Stack.Screen name="Exalmar" component={EmbarcacionesScreen}  options={{ title: Exalmar }}
              />
             </Stack.Navigator>
         </NavigationContainer>
