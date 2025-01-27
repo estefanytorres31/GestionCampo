8bcba9c810b23de187db7f269d8002a759044f9f
Angierc25 <carhuas2005@gmail.com>
2025-01-27 11:59:14 -0500

    embarcaciones

--- AppGestion/Navigation.jsx
+++ AppGestion/Navigation.jsx
@@ -3,15 +3,26 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LoginScreen from "./screens/views/Auth/Login";
 import ClientScreen from "./screens/client/Client";
+import EmbarcacionesScreen from "./screens/embarcacion/Embarcacion";
+import { TouchableOpacity } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 const Stack = createNativeStackNavigator();
 
 export default function Navigation () {
     return (
         <NavigationContainer >
-            <Stack.Navigator initialRouteName="Clientes"  screenOptions={{ headerShown: false }}>
+            <Stack.Navigator initialRouteName="Exalmar"  screenOptions={{ headerShown: false }}>
                 <Stack.Screen name="Login" component={LoginScreen} />
                 <Stack.Screen name="Clientes" component={ClientScreen}  options={{ headerShown: true }} />
+                <Stack.Screen name="Exalmar" component={EmbarcacionesScreen}  options={{
+                headerShown: true,
+                headerTitle: "Exalmar",
+                headerLeft: () => (
+                <Ionicons name="chevron-back" size={24} color="black" />
+                ),
+                }}
+             />
             </Stack.Navigator>
         </NavigationContainer>
     );
