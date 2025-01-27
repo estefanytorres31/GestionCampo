06773b77325ee2ed83ec671bab0db029765fb5de
Estefany Torres <estefanyt3101@gmail.com>
2025-01-27 15:56:30 -0500

    Nuevo

--- back/src/routes/UsuarioRoute.js
+++ back/src/routes/UsuarioRoute.js
@@ -7,10 +7,11 @@ import {
     updateUser
 } from "../controllers/UsuarioController.js";
 import { Router } from "express";
+import {verificarAuth } from "../middleware/verificarAuth.js"
 
 const usuarioRouter = Router();
 
-usuarioRouter.get('/api/usuario', getAllUsers);
+usuarioRouter.get('/api/usuario', verificarAuth, getAllUsers);
 usuarioRouter.get('/api/usuario/:nombre_usuario', getUserByUsername);
 usuarioRouter.post('/api/usuario', createUsuario);
 usuarioRouter.get('/api/usuario/:id', getUserById);
