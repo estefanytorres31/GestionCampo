f01d8d9c232a4a37d6cfcdd89659e305948966a9
Estefany Torres <estefanyt3101@gmail.com>
2025-01-29 15:30:50 -0500

    Nuevo

--- back/src/services/UsuarioService.js
+++ back/src/services/UsuarioService.js
@@ -215,16 +215,3 @@ export const deleteUser = async (id) => {
     return deletedUser;
 };
 
-export const getAllUsers=async()=>{
-    const users = await prisma.usuario.findMany({
-        where: { estado: true },
-        include: {
-            usuario_roles: {
-                include: {
-                    rol: true,
-                },
-            },
-        },
-    });
-    return users;
-}
