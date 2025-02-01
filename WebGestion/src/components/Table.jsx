50fb47c6b9a95e5c41de96c5239f8642710e71b0
yoiberdev <yoiber696@gmail.com>
2025-02-01 11:32:17 -0500

    🐛 FIX: dasd

--- WebGestion/src/components/Table.jsx
+++ WebGestion/src/components/Table.jsx
@@ -2,29 +2,30 @@ import React from "react";
 
 export const Table = ({ columns, data, render }) => {
   return (
-    <>
-      <table>
-        <thead>
-          <tr className="bg-gray-400 text-white rounded-t-lg">
-            {columns?.map(({ name, uuid }) => (
-              <th key={uuid} className={uuid}>
-                {name || uuid}
-              </th>
+    <table>
+      <thead>
+        <tr className="bg-gray-400 text-white rounded-t-lg">
+          {columns?.map(({ name, uuid }) => (
+            <th key={uuid} className={uuid}>
+              {name || uuid}
+            </th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((row, rowIndex) => (
+          <tr
+            key={row.id_entrada || rowIndex} // Usar ID único o índice como fallback
+            className="border-b border-gray-200 hover:bg-gray-100 transition-colors duration-300"
+          >
+            {columns.map(({ uuid }) => (
+              <td key={`${row.id_entrada || rowIndex}-${uuid}`} className={uuid}>
+                {render && render[uuid] ? render[uuid](row) : row[uuid]}
+              </td>
             ))}
           </tr>
-        </thead>
-        <tbody>
-          {data.map((row) => (
-            <tr key={row.id} className="border-b border-gray-200 hover:bg-gray-100 transition-colors duration-300">
-              {columns.map(({ uuid }) => (
-                <td key={uuid} className={uuid}>
-                  {render && render[uuid] ? render[uuid](row) : row[uuid]}
-                </td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </>
+        ))}
+      </tbody>
+    </table>
   );
 };
--- WebGestion/src/layouts/Header.jsx
+++ WebGestion/src/layouts/Header.jsx
@@ -3,10 +3,14 @@ import React from "react";
 const Header = ({ title }) => {
   return (
     <>
-    <header className="fixed top-0 left-0 right-0 h-16 bg-gray-900 flex items-center justify-between border-b border-teal-500"></header>
-      <header className="header-layout bg-gray-900 flex items-center justify-between z-5 border-b border-teal-500">
+    <header className="fixed top-0 left-0 right-0 h-16 bg-white flex items-center justify-between"
+    style={{
+      boxShadow: "0px 6px 24px 0px rgba(18, 96, 44, 0.08)", // Sombra en el borde inferior
+      borderBottom: "1px solid rgba(18, 85, 42, 0.12)", // Borde inferior sutil
+    }}></header>
+      <header className="header-layout bg-white flex items-center justify-between z-5 ">
         <section className="flex justify-start items-center flex-1 self-stretch gap-2">
-          <h1 className="text-lg font-semibold text-white">{title}</h1>
+          <h1 className="text-lg font-semibold text-black">{title}</h1>
         </section>
       </header>
     </>
